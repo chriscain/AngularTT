9bebf6c179d78550ad8ef0d1cde1f3c4adde5e28
chrishcain <k09cc01@kzoo.edu>
2014-02-11 20:31:30 -0500

    first commit

---
+++ public/js/directives/talentDirectives.js
@@ -0,0 +1,63 @@
+myApp.directive('ngRightClick', function($parse) {
+	return function(scope, element, attrs) {
+		var fn = $parse(attrs.ngRightClick);
+		element.bind('contextmenu', function(event) {
+			scope.$apply(function() {
+				event.preventDefault();
+				fn(scope, {$event:event});
+			});
+		});
+	};
+});
+
+myApp.directive('resize', function ($window) {
+	return function (scope, element, attrs) {
+		var theWindow = angular.element($window);
+		scope.getWindowDimensions = function () {
+			return { 'height': theWindow.height(), 'width': theWindow.width() };
+		};
+
+		scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+
+			scope.style = function () {
+
+				if (newValue.width > 1185)
+				{
+					return {
+						'height': (newValue.height) + 'px',
+					};
+				}
+			};
+
+		}, true);
+
+		theWindow.bind('resize', function () {
+			scope.$apply();
+		});
+	};
+});
+
+myApp.directive('activateMe', function () {
+	return function (scope, element, attrs) {
+
+		//generate groups
+		if (scope.groups[attrs.activeGroup]) {
+			scope.groups[attrs.activeGroup].push(element);
+		} else {
+			scope.groups[attrs.activeGroup] = [];
+			scope.groups[attrs.activeGroup].push(element);
+		}
+
+		if (attrs.firstActive) {
+			scope.groups[attrs.activeGroup][attrs.firstActive].addClass("active");
+		}
+
+		//remove active classes from all and add to clicked item
+		element.bind('click', function() {
+			scope.groups[attrs.activeGroup].forEach(function(item) {
+				item.removeClass("active");
+			});
+			element.addClass("active");
+		});
+	};
+});
---
+++ public/js/services/rowService.js
@@ -0,0 +1,67 @@
+myApp.service('rowService', function() {
+
+	var treeRows = [];
+	var allTalents = [];
+
+	return {
+		generateRows: function(rows, columns){
+
+			for (var i = 0; i < rows; i++)
+			{
+				//push our first row object onto the array of row objects
+				treeRows[i] = {
+
+					row_num: i,
+					//our object contains one property, columns, that is an array of column objects.
+					columns: []
+				};
+				for (var j = 0; j < columns; j++)
+				{
+					var col = {
+						pointsIn: 0,
+						maxPoints: '1',
+						imageURL: 'http://demandbaselabs.com/ga/images/delete-icon.png',
+						talentName: 'Untitled ' + (allTalents.length + 1),
+						description: 'My Beautiful Description',
+						row_id: i + 1,
+						col_id: j + 1,
+						disabled: false,
+						per: 1,
+						prereq: 'None'
+					};
+					treeRows[i].columns.push(col);
+					allTalents.push(col);
+				}
+			}
+		},
+
+		addRow: function(columns){
+			treeRows.push({
+				columns: []
+			});
+			var lastIndex = treeRows.length - 1;
+			for (var j = 0; j < columns; j++)
+			{
+				var col = {
+					pointsIn: 0,
+					maxPoints: 1,
+					imageURL: 'http://demandbaselabs.com/ga/images/delete-icon.png',
+					talentName: 'Untitled ' + (allTalents.length+1),
+					row_id: lastIndex + 1,
+					col_id: j + 1,
+					disabled: false,
+					per: 1,
+					prereq: 'None'
+				};
+				treeRows[lastIndex].columns.push(col);
+				allTalents.push(col);
+			}
+		},
+
+		removeRow: function() {
+			treeRows.pop();
+		},
+		treeRows: treeRows,
+		talents: allTalents
+	};
+});
